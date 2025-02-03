7729757d8ae9fde1418aa6ead1df93e3c1f8888f
navead kazi <navead@carwale.com>
2025-02-03 21:34:29 +0530

    Update the build docker script

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,19 @@
 FROM reactnativecommunity/react-native-android:v15.0
 
-#ARG VERSION_NAME
-
+# set a working directory for andoid app
 WORKDIR /android-app
 
-#Copy dependency files and install
+#Copy dependency files and install, since that
+# allows Docker to cache this step if package.json did not change
 COPY package.json .
 COPY package-lock.json .
 RUN npm install
 
-# Add code files
+# Add code files (.dockerignore files will not be copied)
 COPY . .
 
 # On startup, execute the build
+# we neeed to pass the new version Name, version code will be autoincremented
 RUN chmod +x ./android/build-inside-docker.sh
 ENTRYPOINT ["./android/build-inside-docker.sh"]
 
--- android/build-inside-docker.sh
+++ android/build-inside-docker.sh
@@ -1,41 +1,43 @@
 #!/bin/bash -e
 
-# Pass in build number, so we can adjust version
-VERSION_NAME="$1"
-if [ -z "$VERSION_NAME" ]; then
-        echo "usage: $0 <VERSION_NAME>"
-        exit 1
-fi
-
 # Pretty output of log msgs with yellow color
 function msg {
         echo -e "\e[1;33m[APP-BUILD]\e[0m $1"
 }
 
-# Install dependencies and build React Native code
-#msg "(1/5) Build React Native code"
-#yarn
-#yarn android-build
+# Default build command
+BUILD_COMMAND=${1:-bundleRelease}
 
 # Gradle clean
-msg "(2/5) Gradle clean"
+msg "(1/3) Gradle clean"
 cd android
 ./gradlew clean
 
-# Update version coed and version name
-msg "(3/5) Update version"
-chmod +x ./update-android-version.sh
-./update-android-version.sh ${VERSION_NAME}
+# Detect build type (Debug or Release)
+BUILD_TYPE=$(echo "$BUILD_COMMAND" | grep -Eo 'Debug|Release' || echo "Release")
+
+# Detect flavor (if any)
+# Extract Flavor (anything between 'assemble' or 'bundle' and 'Type')
+FLAVOR=$(echo "$BUILD_COMMAND" | sed -E 's/^(assemble|bundle)(.*)?(Debug|Release)$/\2/' | sed 's/^$/default/')
+
+# Determine default output filename if not provided
+if [[ "$BUILD_COMMAND" == *"bundle"* ]]; then
+        OUTPUT_PATH="app/build/outputs/bundle/${FLAVOR,,}/${BUILD_TYPE,,}"
+elif [[ "$BUILD_COMMAND" == *"assemble"* ]]; then
+        OUTPUT_PATH="app/build/outputs/apk/${FLAVOR,,}/${BUILD_TYPE,,}"
+else
+        msg "Unknown build type, skipping output file check."
+        exit 0
+fi
 
 # Build Android app using Gradle
-msg "(4/5) Gradle build"
-./gradlew bundleRelease
+msg "(2/3) Gradle $BUILD_COMMAND"
+./gradlew "$BUILD_COMMAND"
 
 # Sanity check that build was created
-outfile="$(pwd)/app/build/outputs/bundle/release/app-release.aab"
-msg "(5/5) Check .aab was created"
-if [ -f "$outfile" ]; then
-        echo "SUCCESS: Output is here: $outfile"
+msg "(7/3) Checking if output file was created"
+if [ -f "$OUTPUT_PATH" ]; then
+        echo "SUCCESS: Output is here: $OUTPUT_PATH"
 else
         echo "ERROR: Output does NOT exist"
         exit 99
